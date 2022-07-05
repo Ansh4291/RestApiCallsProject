e090e01eaaad693b5481cf0aa8528b92f3e80543
AnshulRathore <anshulrathore1995@gmail.com>
2022-07-05 20:18:53 +0530

    Added:Hello message Added

---
+++ src/main/java/com/example/firstspringapp/controller/HelloRestApi.java
@@ -0,0 +1,12 @@
+//package com.example.firstspringapp.controller;
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class HelloRestApi {
+//    @RequestMapping("/hello")
+//    public String Hello (){
+//        return "hello from bridgelabz";
+//    }
+//}
