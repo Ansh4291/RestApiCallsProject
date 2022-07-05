dc4b1cf763e3085d64f3ecf15ea65b09d0b93e5d
AnshulRathore <anshulrathore1995@gmail.com>
2022-07-05 21:24:39 +0530

    Added:Rest Call With Name Parameter

--- src/main/java/com/example/firstspringapp/FirstSpringAppApplication.java
+++ src/main/java/com/example/firstspringapp/FirstSpringAppApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FirstSpringAppApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(FirstSpringAppApplication.class, args);
+        System.out.println("hello");
     }
 
 }
--- src/main/java/com/example/firstspringapp/controller/HelloRestApi.java
+++ src/main/java/com/example/firstspringapp/controller/HelloRestApi.java
@@ -1,12 +1,15 @@
 package com.example.firstspringapp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloRestApi {
     @RequestMapping("/hello")
-    public String Hello (){
+    public String Hello() {
         return "hello from bridgelabz";
+    }
+    @RequestMapping(value = {"/query/{name}"}, method = RequestMethod.GET)
+    public String sayHello(@PathVariable String name) {
+        return "Hello " + name + "!";
     }
 }
